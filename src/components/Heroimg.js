91e436b67692bcbd1c09abeacc3634ff798c1c94
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-23 06:59:01 +0200

    Update: removed class from exported component

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,7 +1,7 @@
 const Heroimg = (props) => {
     return(
         
-        <img className="hero__img" src={props.src} alt={props.alt}/>
+        <img src={props.src} alt={props.alt}/>
         
     )
 }
