c62adcc420df0037856c9feb79848216e7db2e96
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-23 04:15:38 +0200

    feat: header created with logo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# live preview link
 https://maker-pre-launch-landing-page.netlify.app/
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/header/Header.js
@@ -0,0 +1,12 @@
+import logo from '../assets/logo.svg'
+
+const Header = () => {
+    return(
+        <>
+            <nav>
+                <img src={logo} alt="maker logo"/>
+            </nav>
+        </>
+    )
+}
+export default Header
