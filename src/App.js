d30cdd8601b64b30782adc271bc9938aa8c72448
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-23 20:37:33 +0200

    git fix ref errors

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/style.css';
+import Header from './header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
